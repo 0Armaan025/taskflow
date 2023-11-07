a5412760a0551bd63850cc13d70f2a83c464e957
0Armaan025 <armaan33000@gmail.com>
2023-11-07 21:05:55 +0530

    updated theme

---
+++ taskflow/lib/constants/Pallete.dart
@@ -0,0 +1,11 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class Pallete {
+  final Color bgColor = HexColor("#fffffe");
+  final Color headingTextColor = HexColor("#272343");
+  final Color paragraphTextColor = HexColor('#2d334a');
+  final Color buttonColor = HexColor("#ffd803");
+  final Color buttonTextColor = HexColor("#272343");
+}
